618e1d422024f140ef0cac3aade475c73cbacca5
Gabrrpr <johngbatac@gmail.com>
2026-06-12 21:57:55 +0800

    test: currencyConversion

---
+++ apps/web/src/context/CurrencyContext.jsx
@@ -0,0 +1,5 @@
+// Compatibility wrapper: some components import the correctly-spelled CurrencyContext
+// while the existing implementation lives in CuurencyContext.jsx (typo).
+
+export * from "./CuurencyContext";
+
